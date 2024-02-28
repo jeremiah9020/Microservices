25ede1e43827c4983ef81a4b67339dbaa7bab510
Jeremiah Lynn <jeremiah9020@gmail.com>
2024-02-28 09:23:58 -0800

    updated api

--- service/src/routes/index.js
+++ service/src/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('Index! v0.0');
+  res.send('Index! v1.0');
 });
 
 module.exports = router;
