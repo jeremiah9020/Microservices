ee5cc35ef5f7f476600cd183740b1d5f3149c173
Jeremiah Lynn <jeremiah9020@gmail.com>
2024-02-27 17:58:49 -0800

    updated scripts

--- scripts/create_service.sh
+++ scripts/create_service.sh
@@ -1,16 +1,18 @@
 cd "$(dirname "$(realpath -- "$0")")";
 
-docker build --tag recipescr.azurecr.io/service --platform linux/amd64 ../service/src
+DATE="$(date +"%Y-%m-%d-%H-%M-%S")"
+
+docker build --tag recipescr.azurecr.io/service:$DATE --platform linux/amd64 ../service/src
 
 az acr login --name recipescr   
 
-docker push recipescr.azurecr.io/service      
+docker push recipescr.azurecr.io/service:$DATE   
 
 az containerapp create \
     --name service \
     --resource-group recipesrg \
     --environment recipesenv \
-    --image recipescr.azurecr.io/service \
+    --image recipescr.azurecr.io/service:$DATE \
     --target-port 3000 \
     --ingress 'external' \
     --registry-server recipescr.azurecr.io \
--- scripts/update_service.sh
+++ scripts/update_service.sh
@@ -2,14 +2,14 @@ cd "$(dirname "$(realpath -- "$0")")";\
 
 DATE="$(date +"%Y-%m-%d-%H-%M-%S")"
 
-docker build --tag recipescr.azurecr.io/service-$DATE --platform linux/amd64 ../service/src
+docker build --tag recipescr.azurecr.io/service:$DATE --platform linux/amd64 ../service/src
 
 az acr login --name recipescr   
 
-docker push recipescr.azurecr.io/service-$DATE   
+docker push recipescr.azurecr.io/service:$DATE   
 
 az containerapp update \
   --name service \
-  --resource-group recipes-rg \
-  --image recipescr.azurecr.io/service-$DATE \
+  --resource-group recipesrg \
+  --image recipescr.azurecr.io/service:$DATE \
   --query properties.configuration.ingress.fqdn
