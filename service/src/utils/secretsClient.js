8e595cb4b19e264d71262c2bb2cbc75c50518257
Jeremiah Lynn <jeremiah9020@gmail.com>
2024-02-29 14:25:44 -0800

    Trying to figure out why this isn't working...

--- service/src/utils/secretsClient.js
+++ service/src/utils/secretsClient.js
@@ -1,7 +1,6 @@
 const { DefaultAzureCredential } = require("@azure/identity");
 const { SecretClient } = require("@azure/keyvault-secrets");
 
-console.log(process.env)
 const credential = new DefaultAzureCredential();
 const url = process.env.AZURE_KEYVAULT_RESOURCEENDPOINT;
 const client = new SecretClient(url, credential);
