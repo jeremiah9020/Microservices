65920841543774808227bddf23f31d9e4712cd23
Jeremiah Lynn <jeremiah9020@gmail.com>
2024-02-29 14:09:34 -0800

    added a few logs

--- service/src/utils/secretsClient.js
+++ service/src/utils/secretsClient.js
@@ -2,7 +2,11 @@ const { DefaultAzureCredential } = require("@azure/identity");
 const { SecretClient } = require("@azure/keyvault-secrets");
 
 const credential = new DefaultAzureCredential();
+console.log(credential);
+
 const url = process.env.AZURE_KEYVAULT_RESOURCEENDPOINT;
+
+console.log(url);
 const client = new SecretClient(url, credential);
 
 module.exports = client;
