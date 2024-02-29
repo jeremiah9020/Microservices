4ea728c1996a13e500562fafcef6e5eb222f9e2c
Jeremiah Lynn <jeremiah9020@gmail.com>
2024-02-29 14:41:18 -0800

    changed env variable

--- service/src/utils/secretsClient.js
+++ service/src/utils/secretsClient.js
@@ -2,7 +2,7 @@ const { DefaultAzureCredential } = require("@azure/identity");
 const { SecretClient } = require("@azure/keyvault-secrets");
 
 const credential = new DefaultAzureCredential();
-const url = process.env.AZURE_KEYVAULT_RESOURCEENDPOINT;
+const url = process.env.KEYVAULT;
 const client = new SecretClient(url, credential);
 
 module.exports = client;
