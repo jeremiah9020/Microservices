e2764396b8a55015574c652203bf300ad1213e17
Jeremiah Lynn <jeremiah9020@gmail.com>
2024-02-29 17:52:56 -0800

    please work...

--- service/src/application/routes/index.js
+++ service/src/application/routes/index.js
@@ -6,16 +6,13 @@ const { DaprClient, HttpMethod } = require('@dapr/dapr');
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  console.log(DaprClient);
-  console.log(HttpMethod);
-
-  // const daprHost = "127.0.0.1";
-  // const daprPort = "3000";
-  // const client = new DaprClient({ daprHost, daprPort });
-  // const serviceAppId = "service2";
-  // const serviceMethod = "/";
-  // const response = await client.invoker.invoke(serviceAppId, serviceMethod, HttpMethod.GET);
-  res.send("123")
+  const daprHost = "127.0.0.1";
+  const daprPort = "3000";
+  const client = new DaprClient({ daprHost, daprPort });
+  const serviceAppId = "service2";
+  const serviceMethod = "/";
+  const response = await client.invoker.invoke(serviceAppId, serviceMethod, HttpMethod.GET);
+  res.send(response)
 });
 
 module.exports = router;
