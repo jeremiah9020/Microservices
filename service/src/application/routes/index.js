a81e2e8129a5b3ac8781bf773eb0f1f378afdbe0
Jeremiah Lynn <jeremiah9020@gmail.com>
2024-02-29 18:27:34 -0800

    Updated service

--- service/src/application/routes/index.js
+++ service/src/application/routes/index.js
@@ -6,20 +6,8 @@ const axios = require('axios');
 /* GET home page. */
 router.get('/', async function(req, res, next) {
   // Make a request for a user with a given ID
-  axios.get('https://service2.happyfield-2bbfce7e.westus.azurecontainerapps.io')
-    .then(function (response) {  
-      // handle success
-      console.log(response.data);
-    })
-    .catch(function (error) {
-      // handle error
-      console.error(error);
-    })
-    .finally(function () {
-      res.send('123');
-    });
+  const response = await axios.get('https://service2.happyfield-2bbfce7e.westus.azurecontainerapps.io')
+  res.send("response from service2: " + JSON.stringify(response));
 });
 
 module.exports = router;
-
-
