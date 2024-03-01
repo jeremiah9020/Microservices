d15d851d24546e610abb5d66d5d50ba05437f32c
Jeremiah Lynn <jeremiah9020@gmail.com>
2024-03-01 11:13:36 -0800

    Updated service

--- service/src/application/routes/index.js
+++ service/src/application/routes/index.js
@@ -7,9 +7,14 @@ router.get('/', async function(req, res, next) {
   console.log(serviceBridge);
 
   let json = {};
-;  if (serviceBridge) {
-    const response = await serviceBridge('service2', '/')
-    json = await response.json();
+  if (serviceBridge) {
+    try {
+      const response = await serviceBridge('service2', '/')
+      json = await response.json();
+    }
+    catch (error) {
+      console.error(error)
+    }
   }
 
   res.send("response from service2(?): " + JSON.stringify(json));
