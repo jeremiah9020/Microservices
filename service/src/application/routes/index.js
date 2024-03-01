db33d3640689399f53cdb1521154a54d333c6da0
Jeremiah Lynn <jeremiah9020@gmail.com>
2024-02-29 17:18:22 -0800

    Testing

--- service/src/application/routes/index.js
+++ service/src/application/routes/index.js
@@ -2,20 +2,20 @@ var express = require('express');
 var router = express.Router();
 const { DaprClient, HttpMethod } = require('@dapr/dapr');
 
-console.log(DaprClient);
-console.log(HttpMethod);
-
 // const db = require('../../database/db');
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  const daprHost = "127.0.0.1";
-  const daprPort = "3000";
-  const client = new DaprClient({ daprHost, daprPort });
-  const serviceAppId = "service2";
-  const serviceMethod = "/";
-  const response = await client.invoker.invoke(serviceAppId, serviceMethod, HttpMethod.GET);
-  res.send(response)
+  console.log(DaprClient);
+  console.log(HttpMethod);
+
+  // const daprHost = "127.0.0.1";
+  // const daprPort = "3000";
+  // const client = new DaprClient({ daprHost, daprPort });
+  // const serviceAppId = "service2";
+  // const serviceMethod = "/";
+  // const response = await client.invoker.invoke(serviceAppId, serviceMethod, HttpMethod.GET);
+  res.send("123")
 });
 
 module.exports = router;
