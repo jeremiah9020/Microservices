0e60879a381d66a72586a361ecedee98808b8226
Jeremiah Lynn <jeremiah9020@gmail.com>
2024-02-29 16:30:29 -0800

    Added service2

---
+++ service2/src/database/db.js
@@ -0,0 +1,57 @@
+const { Sequelize } = require('sequelize');
+
+
+/**
+ * Sets up the database and table definitions
+ * @type {Promise<Sequelize>}
+ */
+const sequelize = new Promise(async (res) => {
+    console.log('Getting database')
+
+    const db = await ((process.env.ONLINE) ? getProductionDatabase() : getLocalDatabase());
+
+    const modelDefiners = [
+        require('./model/book.model'),
+        // Add more models here...
+    ];
+
+    for (const modelDefiner of modelDefiners) {
+        modelDefiner(db);
+    }
+
+    res(db);
+})
+
+async function getProductionDatabase() {
+    console.log('Getting production database');
+
+    const secretsClient = require('../utils/secretsClient');
+    const db_username = await secretsClient.getSecret('DB-USERNAME')
+    const db_password = await secretsClient.getSecret('DB-PASSWORD')
+    const db_host = await secretsClient.getSecret('DB-HOST')
+
+    return new Sequelize(
+        process.env.DB, 
+        db_username.value, 
+        db_password.value, 
+        {
+            logging:false,
+            host:db_host.value,
+            dialect: 'mssql',
+            dialectOptions: {
+                encrypt: true
+            }
+        }
+    );
+}
+
+async function getLocalDatabase() {
+    console.log('Getting local database');
+
+    return new Sequelize('sqlite::memory:',{
+        logging: false
+    });
+}
+
+
+module.exports = sequelize;
---
+++ service2/src/utils/databaseConnection.js
@@ -0,0 +1,17 @@
+const { Sequelize } = require("sequelize");
+
+/**
+ * Checks if the database is connected
+ * @param {Sequelize} db 
+ */
+module.exports = async function isDatabaseConnected(db) {
+	console.log('Checking database connection');
+	try {
+		await db.authenticate();
+		console.log('Database connection OK!');
+	} catch (error) {
+		console.error('Unable to connect to the database:');
+		console.error(error.message);
+		process.exit(1);
+	}
+}
