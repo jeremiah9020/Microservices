eea238ef586dad0dcf753a6d0d4563fd99c8d4dd
Jeremiah Lynn <jeremiah9020@gmail.com>
2024-03-01 13:24:09 -0800

    Removed unused

--- service2/src/utils/secretsClient.js
+++
@@ -1,8 +0,0 @@
-const { DefaultAzureCredential } = require("@azure/identity");
-const { SecretClient } = require("@azure/keyvault-secrets");
-
-const credential = new DefaultAzureCredential();
-const url = process.env.KEYVAULT;
-const client = new SecretClient(url, credential);
-
-module.exports = client;
