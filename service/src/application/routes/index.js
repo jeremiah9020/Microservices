e2d9783d5b3025bd381ceb13e19447df86d45887
Jeremiah Lynn <jeremiah9020@gmail.com>
2024-02-29 18:24:01 -0800

    fixed bug

--- service/src/application/routes/index.js
+++ service/src/application/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-const axios = require('axios/index.cjs');
+const axios = require('axios');
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
