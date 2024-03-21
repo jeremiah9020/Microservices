b8b4a6bffc9d42e3df029fb4ff9d64df390b4bd5
Jeremiah Lynn <jeremiah9020@gmail.com>
2024-03-20 21:51:03 -0700

    Added roles services

--- shared/index.js
+++ shared/index.js
@@ -1,4 +1,18 @@
 const serviceBridge = require('./utils/serviceBridge')
 const secretsClient = require('./utils/secretsClient')
 const { getLocalDatabase, getProductionDatabase, isDatabaseConnected} = require('./utils/database')
-module.exports = { serviceBridge, secretsClient, database: { getLocalDatabase, getProductionDatabase, isDatabaseConnected }}
+const { getRoleNumber, getRoleObject } = require('./utils/roles')
+
+module.exports = { 
+    serviceBridge, 
+    secretsClient, 
+    database: { 
+        getLocalDatabase, 
+        getProductionDatabase, 
+        isDatabaseConnected 
+    },
+    role: {
+        getRoleNumber,
+        getRoleObject
+    }
+}
---
+++ shared/utils/roles.js
@@ -0,0 +1,65 @@
+/****************************************
+*               ROLE TABLE              *
+*                                       *
+* 1 - Can see private posts             *
+* 2 - Can see private cookbooks         *
+* 4 - Can see user roles                *
+* 8 - Can delete posts                  *
+* 16 - Can delete cookbooks             *
+* 32 - Can adjust user roles            *
+* 64 - Can suspend users                *
+* 128 - Can delete users                *
+****************************************/
+
+/** @typedef {{ canSeePrivatePosts: boolean, canSeePrivateCookbooks: boolean, canSeeRoles: boolean, canDeletePosts: boolean, canDeleteCookbooks: boolean, canAdjustRoles: boolean, canSuspendUsers: boolean, canDeleteUsers: boolean}} RoleObject */
+
+/**
+ * Converts a role object into a role number
+ * @param {RoleObject} roleObject 
+ * @returns {number} the role number
+ */
+const getRoleNumber = (roleObject) => {
+    let role = 0;
+
+    if (roleObject.canSeePrivatePosts) role |= 1
+    if (roleObject.canSeePrivateCookbooks) role |= 2
+    if (roleObject.canSeeRoles) role |= 4
+    if (roleObject.canDeletePosts) role |= 8
+    if (roleObject.canDeleteCookbooks) role |= 16
+    if (roleObject.canAdjustRoles) role |= 32
+    if (roleObject.canSuspendUsers) role |= 64
+    if (roleObject.canDeleteUsers) role |= 128
+
+    return role;
+}
+
+/**
+ * Converts a role number into a role object
+ * @param {number} roleNumber 
+ * @returns {RoleObject} the role object
+ */
+const getRoleObject = (roleNumber) => {
+    let role = {
+        canSeePrivatePosts: false,
+        canSeePrivateCookbooks: false,
+        canSeeRoles: false,
+        canDeletePosts: false,
+        canDeleteCookbooks: false,
+        canAdjustRoles: false,
+        canSuspendUsers: false,
+        canDeleteUsers: false
+    };
+
+    if (roleNumber ^ 1) roleObject.canSeePrivatePosts = true;
+    if (roleNumber ^ 2) roleObject.canSeePrivateCookbooks = true;
+    if (roleNumber ^ 4) roleObject.canSeeRoles = true;
+    if (roleNumber ^ 8) roleObject.canDeletePosts = true;
+    if (roleNumber ^ 16) roleObject.canDeleteCookbooks = true;
+    if (roleNumber ^ 32) roleObject.canAdjustRoles = true;
+    if (roleNumber ^ 64) roleObject.canSuspendUsers = true;
+    if (roleNumber ^ 128) roleObject.canDeleteUsers = true;
+
+    return role;
+}
+
+module.exports = { getRoleNumber, getRoleObject };
