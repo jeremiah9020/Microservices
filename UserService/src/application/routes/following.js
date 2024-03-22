4b75adb31a278ac3b1ea2c883761b31eb6700023
Jeremiah Lynn <jeremiah9020@gmail.com>
2024-03-22 14:12:31 -0700

    Updated following

--- UserService/src/application/routes/following.js
+++ UserService/src/application/routes/following.js
@@ -1,24 +1,81 @@
 const express = require('express');
 const router = express.Router();
+const sequelize = require('../../database/db');
+const { authenticate } = require('shared');
+
+async function updateFollowingList(user, add, remove) {
+  const following = JSON.parse(user.following);
+
+  const db = await sequelize;
+
+
+  if (remove) {
+    for (const toRemove of remove) {
+      try {
+        await db.transaction(async (t) => {
+          const removedUser = await db.models.user.findByPk(toRemove, { transaction: t });
+          await removedUser.update({followers: user.followers - 1}, {transaction: t})
+        });
+      } catch (err) {
+        console.log(err)
+      }
+     
+      const index = following.indexOf(toRemove);
+      following.splice(index, 1);
+    }
+  }
+
+  if (add) {
+    for (const toAdd of add) {
+      try {
+        await db.transaction(async (t) => {
+          const addedUser = await db.models.user.findByPk(toAdd, { transaction: t });
+          await addedUser.update({followers: user.followers + 1}, {transaction: t})
+        });
+
+        following.push(toAdd);
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  }
+
+  return await user.update({following: JSON.stringify(following)});
+}
+
 
 /**
  * Used to add or remove a user from the following list.
  */
-router.patch('/', async function(req, res, next) {
-  const { id, add, remove } = req.body;
+router.patch('/', authenticate.strictly, async function(req, res, next) {
+  const { username, add, remove } = req.body;
+  
+  const db = await sequelize;
 
-  if (id == null) {
-    return res.status(400).json(`Missing request body parameters`);
-  }
+  try {    
+    if (req.fromServer) {
+      if (username) {
+        const user = await db.models.user.findByPk(username);
+        await updateFollowingList(user, add, remove)
 
-  // successfully updated the user's following list
-  return res.status(200).send();
+        // successfully updated the user's following list
+        return res.status(200).send();
+      } else {
 
-  // lacking authorization to update content
-  return res.status(403).json({error: 'lacking authorization to update the user\'s following list'});
+        // missing username parameter
+        return res.status(400).json(`Missing request body parameters`);
+      }
+    } else {
+      const user = await db.models.user.findByPk(req.username);
+      await updateFollowingList(user, add, remove)
 
-  // could not find the user
-  return res.status(404).json({error: 'could not find the user'});
+      // successfully saved the user data
+      return res.status(200).send();
+    }
+  } catch (err) {
+    // could not find the user
+    return res.status(404).json({error: 'could not find the user'});
+  } 
 });
 
 
