8a0969f3eb6e90bcf0ca0bd7a7078345e2d870c7
Jeremiah Lynn <jeremiah9020@gmail.com>
2024-03-22 00:02:18 -0700

    Added admin account

--- AuthService/src/database/init.js
+++ AuthService/src/database/init.js
@@ -1,4 +1,6 @@
 const {Sequelize} = require('sequelize');
+const bcrypt = require('bcrypt');
+const { getRoleNumber } = require('shared/utils/roles');
 /**
  * 
  * @param {Sequelize} db 
@@ -8,6 +10,33 @@ module.exports = async (db) => {
     try {
         await db.models.auth.sync({ force: !process.env.ONLINE })
         console.log('Models successfully synced');
+
+        console.log('Attempting to create admin account')
+        try {
+            const username = 'admin';
+            const email = 'email@mail.com';
+            const password = 'password';
+            const role = getRoleNumber({
+                canAdjustRoles: true,
+                canDeleteCookbooks: true,
+                canDeletePosts: true,
+                canDeleteUsers: true,
+                canSeePrivateCookbooks: true,
+                canSeePrivatePosts: true,
+                canSeeRoles: true,
+                canSuspendUsers: true
+            });
+
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(password, salt);
+
+            await db.models.auth.create({ username, password: hashedPassword, email, role });
+            console.log('Admin successfully created');
+        } catch (error) {
+            console.error('Unable to create admin:');
+            console.error(error.message);
+            process.exit(1);
+        }
     } catch (error) {
         console.error('Unable to sync the models:');
         console.error(error.message);
