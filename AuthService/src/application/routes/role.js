253f7a4677f30204e74631f385b5b788ddd0269b
Jeremiah Lynn <jeremiah9020@gmail.com>
2024-03-22 00:13:44 -0700

    Added role services

--- AuthService/src/application/routes/role.js
+++ AuthService/src/application/routes/role.js
@@ -1,48 +1,81 @@
 const express = require('express');
 const router = express.Router();
+const sequelize = require('../../database/db');
+const { role: { getRoleObject }, authenticate } = require('shared');
+
 
 /**
  * Gets a user's role data.
  */
-router.get('/', async function(req, res, next) {
+router.get('/', authenticate.strictly, async function(req, res, next) {
   const { user } = req.query;
   
   if (user == null ) {
     return res.status(400).json(`Missing request query parameters`);
   }
 
-  const role = undefined;
+  const db = await sequelize;
+
+  const userByEmail = await db.models.auth.findOne({ where: { email: user }});
+  const userByUsername = await db.models.auth.findByPk(user);
+  const viewingUser = await db.models.auth.findByPk(req.username);
+
+  if (userByEmail == null && userByUsername == null) {
+    return res.status(404).json({error: 'could not find the user'});
+  }
+
+  const foundUser = userByUsername || userByEmail;
+
+  if (foundUser.username != req.username) {
+    const viewerRole = getRoleObject(viewingUser.role);
+    if (!viewerRole.canSeeRoles) {
+      // lacking authorization to view content
+      return res.status(403).json({error: 'lacking authorization to view the recipe'});
+    }
+  }
+  
+  const role = foundUser.role;
 
   // successfully retrieved user's role data
   return res.status(200).json({ role });
-
-  // lacking authorization to view content
-  return res.status(403).json({error: 'lacking authorization to view role data'});
-
-  // could not find the user
-  return res.status(404).json({error: 'could not find the user'});
 });
 
 /**
- * Gets the user's role data.
+ * Updates the user's role data.
  */
-router.put('/', async function(req, res, next) {
+router.put('/', authenticate.strictly, async function(req, res, next) {
   const { user, role } = req.body;
 
   if (user == null || role == null) {
     return res.status(400).json(`Missing request body parameters`);
   }
 
-  const access_token = undefined;
+  const db = await sequelize;
 
-  // successfully updated user's role data
-  return res.status(200).send();
+  const editingUser = await db.models.auth.findByPk(req.username);
+
+  if (editingUser) {
+    const roles = getRoleObject(editingUser.role);
+
+    if (roles.canAdjustRoles) {
+      const userByEmail = await db.models.auth.findOne({ where: { email: user }});
+      const userByUsername = await db.models.auth.findByPk(user);
+
+      if (userByEmail == null && userByUsername == null) {
+        return res.status(404).json({error: 'could not find the user'});
+      }
+    
+      const foundUser = userByUsername || userByEmail;
+
+      await foundUser.update({ role });
+
+      // successfully updated user's role data
+      return res.status(200).send();
+    }    
+  }
 
   // lacking authorization to set content
   return res.status(403).json({error: 'lacking authorization to set role data'});
-
-  // could not find the user
-  return res.status(404).json({error: 'could not find the user'});
 });
 
 module.exports = router;
