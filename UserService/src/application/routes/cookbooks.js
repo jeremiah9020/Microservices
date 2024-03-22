1551dedd420a018764455ac9dab0e576a0982a84
Jeremiah Lynn <jeremiah9020@gmail.com>
2024-03-22 14:18:54 -0700

    Added cookbooks service

--- UserService/src/application/routes/cookbooks.js
+++ UserService/src/application/routes/cookbooks.js
@@ -1,24 +1,46 @@
 const express = require('express');
 const router = express.Router();
+const sequelize = require('../../database/db');
+const { authenticate } = require('shared');
 
 /**
  * Used to add or remove a cookbook from the cookbooks list.
  */
-router.patch('/', async function(req, res, next) {
-  const { id, add, remove } = req.body;
+router.patch('/', authenticate.server, async function(req, res, next) {
+  const { username, add, remove } = req.body;
 
-  if (id == null) {
+  if (username == null) {
     return res.status(400).json(`Missing request body parameters`);
   }
 
-  // successfully updated the user's cookbook list
+  const db = await sequelize;
+
+  const user = await db.models.user.findByPk(username);
+
+  if (user == null) {
+     // could not find the user
+    return res.status(404).json({error: 'could not find the user'});
+  }
+
+  const cookbooks = JSON.parse(user.cookbooks);
+
+  if (remove) {
+    for (const toRemove of remove) {
+      const index = cookbooks.indexOf(toRemove);
+      cookbooks.splice(index, 1);
+    }
+  }
+
+  if (add) {
+    for (const toAdd of add) {
+      cookbooks.push(toAdd);
+    }
+  }
+
+  await user.update({cookbooks: JSON.stringify(cookbooks)});
+
+  // user's cookbooks successfully updated
   return res.status(200).send();
-
-  // lacking authorization to update content
-  return res.status(403).json({error: 'lacking authorization to update the user\'s cookbook list'});
-
-  // could not find the user
-  return res.status(404).json({error: 'could not find the user'});
 });
 
 
