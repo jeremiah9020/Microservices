70270ad582347be0e714fd708b5f8853373d4fc0
Jeremiah Lynn <jeremiah9020@gmail.com>
2024-04-04 08:32:04 -0700

    Added role check to user deletion

--- AuthService/src/application/routes/delete.js
+++ AuthService/src/application/routes/delete.js
@@ -6,36 +6,47 @@ const { authenticate, role: { getRoleObject } } = require('shared');
 const serviceRequest = require('shared/utils/serviceBridge');
 
 /**
- * TODO: Add role check!
- * 
  * Used to delete a user's authentication data.
  */
-router.post('/', async function(req, res, next) {
+router.post('/', authenticate.strictly, async function(req, res, next) {
   const { username, password } = req.body;
 
-  if (username == null || password == null) {
+  if (username == null) {
     return res.status(400).json(`Missing request body parameters`);
   }
 
   const db = await sequelize;
 
-  try {
-    const user = await db.models.auth.findByPk(username);
-    const passwordMatch = await bcrypt.compare(password, user.password);
-    if (!passwordMatch) {
-      return res.status(401).json({error: 'could not authenticate the user with the given credentials.'});
-    }
-
-    await user.destroy();
-
-    await serviceRequest('UserService','/delete',{ method:'post' }, { username })
-
-    // user's authentication data successfully delete
-    return res.status(200).send();
-  } catch (err) {
+  const user = await db.models.auth.findByPk(username);
+  if (user == null) {
     // could not find the user
     return res.status(404).json({error: 'could not find the user'});
   }
+
+  if (req.username && req.username != username) {
+    const response = await serviceRequest('AuthService', `/role?user=${req.username}`, {method: 'get'});
+    const json = await response.json();
+    if (!getRoleObject(json.role).canDeleteUsers) {
+      // not authorized
+      return res.status(403).json({error: 'Lacking authorization to delete user.'});
+    }
+  } else {
+    if (password == null) {
+      return res.status(400).json(`Missing request body parameters`);
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      return res.status(401).json({error: 'could not authenticate the user with the given credentials.'});
+    }
+  }
+
+  await user.destroy();
+  await serviceRequest('UserService','/delete',{ method:'post' }, { username });
+
+  // user's authentication data successfully delete
+  return res.status(200).send();
 });
 
 module.exports = router;
