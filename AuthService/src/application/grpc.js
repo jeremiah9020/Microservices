76b1b4371255197f83963ca709edf1d6e4a03c92
Jeremiah Lynn <jeremiah9020@gmail.com>
2024-04-11 17:08:30 -0700

    Added grpc to auth service

---
+++ AuthService/src/application/grpc.js
@@ -0,0 +1,45 @@
+const { grpc: { def: { auth: authDef } } } = require('shared');
+const grpc = require('@grpc/grpc-js')
+const sequelize = require('../database/db');
+
+
+async function getRole(call, callback) {
+    const { username } = call.request;
+
+    const db = await sequelize;
+
+    const user = await db.models.auth.findOne({ where: { username }});
+
+    let role = 0;
+    if (user) {
+        role = user.role;
+    }
+
+    return callback(null, { role });
+
+}
+
+async function getTimeout(call, callback) {
+    const { username } = call.request;
+
+    const db = await sequelize;
+    
+    const user = await db.models.auth.findByPk(username);
+
+    let timeout = 0;
+    if (user) {
+        timeout = user.timeout_until;
+    }
+
+    return callback(null, { timeout });
+}
+
+function startGRPC() {   
+    // gRPC server setup
+    const grpcServer = new grpc.Server();
+    grpcServer.addService(authDef.Auth.service, { getRole, getTimeout });
+    grpcServer.bindAsync('0.0.0.0:3102', grpc.ServerCredentials.createInsecure() , () => {});
+}
+
+module.exports = { startGRPC }
+
